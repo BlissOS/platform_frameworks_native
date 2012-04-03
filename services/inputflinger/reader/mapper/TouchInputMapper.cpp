9fefaf4e0ba6a91772ec45af678ad18adfe0d113
Edward Peek <edwardpeek@gmail.com>
2012-04-04 06:17:00 +1200

    Add option to disable touchscreen pressure sensitivity from idc files
    Change-Id: I5d351b247a53955bb27429bf159e746a1fbf707e

--- services/inputflinger/reader/mapper/TouchInputMapper.cpp
+++ services/inputflinger/reader/mapper/TouchInputMapper.cpp
@@ -1208,6 +1208,8 @@ void TouchInputMapper::parseCalibration() {
             out.pressureCalibration = Calibration::PressureCalibration::PHYSICAL;
         } else if (*pressureCalibrationString == "amplitude") {
             out.pressureCalibration = Calibration::PressureCalibration::AMPLITUDE;
+        } else if (*pressureCalibrationString == "disable") {
+            out.pressureCalibration = Calibration::PressureCalibration::DISABLE;
         } else if (*pressureCalibrationString != "default") {
             ALOGW("Invalid value for touch.pressure.calibration: '%s'",
                   pressureCalibrationString->c_str());
@@ -1265,6 +1267,9 @@ void TouchInputMapper::resolveCalibration() {
     if (mRawPointerAxes.pressure.valid) {
         if (mCalibration.pressureCalibration == Calibration::PressureCalibration::DEFAULT) {
             mCalibration.pressureCalibration = Calibration::PressureCalibration::PHYSICAL;
+        } else if (mCalibration.pressureCalibration == Calibration::PressureCalibration::DISABLE) {
+            mRawPointerAxes.pressure.valid = false;
+            mCalibration.pressureCalibration = Calibration::PressureCalibration::NONE;
         }
     } else {
         mCalibration.pressureCalibration = Calibration::PressureCalibration::NONE;
